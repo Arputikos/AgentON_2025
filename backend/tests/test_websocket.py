d2f19e088d58b1749948f3b8159110b540544c53
Marcin Kamiński <emkaminsk@gmail.com>
2025-01-25 15:39:15 +0100

    Websocket test

--- backend/tests/test_websocket.py
+++ backend/tests/test_websocket.py
@@ -1,11 +1,13 @@
 import asyncio
 import websockets
 import json
+import pytest
 
 connection_body = {
     "debate_id": "3cde80d8-46f6-4b04-bcbf-bacf0c531ee6"
 }
 
+@pytest.mark.skip(reason="Manual websocket test")
 async def test():
     async with websockets.connect('ws://localhost:8000/debate') as ws:
         # Send JSON formatted message with debate_id
